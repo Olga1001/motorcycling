cfa8fb54cb9be36744a54c5bc1283d57d0fc9488
Olha <ololoevaevaeva@gmail.com>
2020-03-11 12:38:20 +0200

    ready main page

--- app/js/common.js
+++ app/js/common.js
@@ -3,4 +3,15 @@ $(document).ready(function () {
         $(this).find(".select-arrow").toggleClass('active');
         $(this).closest(".select").find(".select-drop").slideToggle(300);
     });
+    $(".burger").click(function () {
+        $(this).toggleClass('active');
+        $(".menu ").slideToggle(300);
+    });
+    if (window.matchMedia("(min-width: 768px) and (max-width: 991px)").matches) {
+        $(".search-btn").click(function (e) {
+            e.preventDefault();
+            $(this).closest(".search").toggleClass('active');
+        });
+    }
+
 });
